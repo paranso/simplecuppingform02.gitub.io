0a0a60e3638473010d22bd85fc296ba6aa26ed29
paranso <cw0303@gmail.com>
2025-01-01 15:00:40 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+// 간단한 JavaScript 예시
+document.addEventListener("DOMContentLoaded", function () {
+  const msg = document.getElementById('message');
+  msg.textContent = "GitHub Pages에서 작동 중!";
+});
